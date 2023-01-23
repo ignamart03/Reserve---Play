0e57f3f9af82fdb1b677fcff6d36b5ff1e1df1c0
ignamart03 <90852344+ignamart03@users.noreply.github.com>
2023-01-23 01:41:03 +0100

    Index commit

---
+++ Página/index.php
@@ -0,0 +1,17 @@
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reserve & Play</title>
+    <script src="index.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
